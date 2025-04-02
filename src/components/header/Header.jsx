5051d5adad20ecf25bf7fba75618c16e8bfa4bac
sahil-suriya <@m416m762>
2025-04-02 12:45:05 +0530

    component resolve

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+export default function Header(){
+    <div id="header">
+    <ContentWrapper>
+        <div className="header">
+            <div className="navbar">
+                <div className="nav-left">
+                    hello
+                </div>
+            </div>
+            <div className="nav-right">
+                <button>GET IN TOUCH</button>
+                <span>Menu</span>
+            </div>
+        </div>
+    </ContentWrapper>
+    </div>
+}

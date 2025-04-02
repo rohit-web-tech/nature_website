110236342556a756abfbec9c1c807b296fa01029
rohit-web-tech <rohitdogra0126@gmail.com>
2025-04-02 13:21:36 +0530

    new changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,19 @@
 import "./styles.css";
 import ContentWrapper from "../contentwrapper/ContentWrapper";
-export default function Header(){
+export default function Header() {
     return (
-        
-    <div id="header">
-    <ContentWrapper>
-        <div className="header">    
-                <div className="nav-left">
-                    Hello
+        <div id="header">
+            <ContentWrapper>
+                <div className="header">
+                    <div className="nav-left">
+                        Hello
+                    </div>
+                    <div className="nav-right">
+                        <button>GET IN TOUCH</button>
+                        <span>Menu</span>
+                    </div>
                 </div>
-            </div>
-            <div className="nav-right">
-                <button>GET IN TOUCH</button>
-                <span>Menu</span>
-            </div>
-    </ContentWrapper>
-    </div>
+            </ContentWrapper>
+        </div>
     )
 }
